4356e8a666a0030eb133f9a449cd5b47451dffe2
Jakub Tyrka <jakub.t.tyrka@gmail.com>
2019-09-03 00:30:14 +0200

    Inline extension and call action as its receiver

--- bookish-preference/src/main/java/pl/draciel/bookish/preference/Extensions.kt
+++ bookish-preference/src/main/java/pl/draciel/bookish/preference/Extensions.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 
 @SuppressLint("ApplySharedPref")
-internal fun SharedPreferences.edit(action: (SharedPreferences.Editor) -> Unit) {
+internal inline fun SharedPreferences.edit(action: SharedPreferences.Editor.() -> Unit) {
     val editor = edit()
     action(editor)
     editor.commit()
